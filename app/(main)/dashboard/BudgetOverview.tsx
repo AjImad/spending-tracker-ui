0c87a55ce1e92213716873f09c3ef90ccd629d85
ajimad <ajbar.imad.99@gmail.com>
2024-04-04 10:35:19 +0200

    Add button for view all budgets

--- app/(main)/dashboard/BudgetOverview.tsx
+++ app/(main)/dashboard/BudgetOverview.tsx
@@ -1,70 +1,84 @@
 import { Progress } from "@/components/ui/progress";
 import ProgressBar from "../components/ProgressBar";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
 
 const BudgetOverview = () => {
   const startDate: Date = new Date("2024-04-01");
   const endDate: Date = new Date("2024-04-30");
 
   return (
-    <div className="w-full inline-block bg-white rounded-xl shadow-md space-y-6 p-4">
-      <div className="text-md font-bold text-slate-800 leading-normal">
-        Budget overview
-      </div>
-      <div className="space-y-1">
-        <p>Entertaiment</p>
-        <div className="flex justify-center items-center space-x-2">
-          <Image
-            src="/entertaiment.svg"
-            alt="Entertaiment"
-            width={40}
-            height={40}
-            sizes="100vw"
-            // style={{ width: "100%", height: "auto" }}
-          />
-          <ProgressBar
-            startDate={startDate}
-            endDate={endDate}
-            barColor="bg-[#00b8d9]"
-          />
+    <div className="w-full inline-block bg-white rounded-xl shadow-md space-y-6">
+      <div className="p-4">
+        <div className="text-md font-bold text-slate-800 leading-normal mb-5">
+          Budget overview
         </div>
-      </div>
-      <div className="space-y-1">
-        <p>Eating out</p>
-        <div className="flex justify-center items-center space-x-2">
-          <Image
-            src="/eatout.svg"
-            alt="Eating out"
-            width={40}
-            height={40}
-            sizes="100vw"
-            // style={{ width: "100%", height: "auto" }}
-          />
-          <ProgressBar
-            startDate={startDate}
-            endDate={endDate}
-            barColor="bg-[#ff3030]"
-          />
+        <div className="space-y-1">
+          <p>Entertaiment</p>
+          <div className="flex justify-center items-center space-x-2">
+            <Image
+              src="/entertaiment.svg"
+              alt="Entertaiment"
+              width={40}
+              height={40}
+              sizes="100vw"
+              // style={{ width: "100%", height: "auto" }}
+            />
+            <ProgressBar
+              startDate={startDate}
+              endDate={endDate}
+              barColor="bg-[#00b8d9]"
+            />
+          </div>
         </div>
-      </div>
+        <div className="space-y-1">
+          <p>Eating out</p>
+          <div className="flex justify-center items-center space-x-2">
+            <Image
+              src="/eatout.svg"
+              alt="Eating out"
+              width={40}
+              height={40}
+              sizes="100vw"
+              // style={{ width: "100%", height: "auto" }}
+            />
+            <ProgressBar
+              startDate={startDate}
+              endDate={endDate}
+              barColor="bg-[#ff3030]"
+            />
+          </div>
+        </div>
 
-      <div className="space-y-1">
-        <p>Fuel</p>
-        <div className="flex justify-center items-center space-x-2">
-          <Image
-            src="/fuel.svg"
-            alt="Fuel"
-            width={40}
-            height={40}
-            sizes="100vw"
-            // style={{ width: "100%", height: "auto" }}
-          />
-          <ProgressBar
-            startDate={startDate}
-            endDate={endDate}
-            barColor="bg-[#ffab00]"
-          />
+        <div className="space-y-1">
+          <p>Fuel</p>
+          <div className="flex justify-center items-center space-x-2">
+            <Image
+              src="/fuel.svg"
+              alt="Fuel"
+              width={40}
+              height={40}
+              sizes="100vw"
+              // style={{ width: "100%", height: "auto" }}
+            />
+            <ProgressBar
+              startDate={startDate}
+              endDate={endDate}
+              barColor="bg-[#ffab00]"
+            />
+          </div>
         </div>
+      </div>
+      <div className="border-t border-dashed p-2 flex justify-end">
+        <Button
+          variant="outline"
+          className="text-neutral-600 text-sm"
+          size="sm"
+        >
+          View all
+          <ChevronRight className="w-4 h-4 ml-1" />
+        </Button>
       </div>
     </div>
   );
